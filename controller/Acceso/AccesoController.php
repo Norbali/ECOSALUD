5dfd4093d693160ebd637f959f43b6be9ad460b9
Norbali <norqui2007@gmail.com>
2025-12-13 16:01:24 -0500

    quitar echo

--- controller/Acceso/AccesoController.php
+++ controller/Acceso/AccesoController.php
@@ -143,8 +143,6 @@
             $obj = new AccesoModel();
 
             $sql = "SELECT * FROM usuarios WHERE documento = '$documento'";
-            echo "<pre>$sql</pre>";
-            exit;
             $usuario = $obj->select($sql);
 
             if(pg_num_rows($usuario)>0){
